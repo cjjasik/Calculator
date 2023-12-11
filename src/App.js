2f32c04604953c8edb2289d0d3ff3c673546989f
Christina Jasik <cjjasik@gmail.com>
2023-12-11 13:03:10 -0500

    To Do added

--- src/App.js
+++ src/App.js
@@ -3,3 +3,11 @@ export default function App() {
         "Hello World"
     )
 }
+
+
+
+/**
+ * Top Level: Card called "Calculator" (state will live here)
+ * - Display
+ * - Input -> grid of buttons (all buttons will affect display)
+ */
